7386704412d9e146b716dfa27854c2ccdbefb121
portfoliO <sivagk314@gmail.com>
2024-03-21 23:46:31 +0530

    22march

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -37,6 +37,7 @@ function App() {
 
   return (
    
+    
         <>
        <BrowserRouter>
        <Navbar/>
--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,4 +1,4 @@
-//import { motion } from "framer-motion"
+
 
 const About = () =>{
     return (
